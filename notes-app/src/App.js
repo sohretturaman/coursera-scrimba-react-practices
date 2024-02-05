4dcb0afef7c7f24c4477467ba78b4a1560dd03ec
şöhrett <sohretturaman@gmail.com>
2024-02-05 15:44:30 +0300

    crud functions has been created

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,94 +1,104 @@
-import React, { useEffect, useState } from "react"
-import Sidebar from "./components/Sidebar"
-import Editor from "./components/Editor"
-import { data } from "./data"
-import Split from "react-split"
-import {nanoid} from "nanoid"
+/** @format */
 
-import styles from './style.module.css'
+import React, { useEffect, useState } from "react";
+import Sidebar from "./components/Sidebar";
+import Editor from "./components/Editor";
+import { data } from "./data";
+import Split from "react-split";
+import { nanoid } from "nanoid";
+
+import styles from "./style.module.css";
 /**
  * Challenge: Spend 10-20+ minutes reading through the code
  * and trying to understand how it's currently working. Spend
- * as much time as you need to feel confident that you 
+ * as much time as you need to feel confident that you
  * understand the existing code (although you don't need
  * to fully understand everything to move on)
  */
 
 export default function App() {
-    let currentvals =JSON.parse( localStorage.getItem('notes'))
-    const [notes, setNotes] = React.useState(currentvals ?currentvals:[])
-    const [currentNoteId, setCurrentNoteId] = React.useState(
-        (notes[0] && notes[0].id) || ""
-    )
+  let currentvals = () => JSON.parse(localStorage.getItem("notes"));
+  const [notes, setNotes] = React.useState(currentvals || []);
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+    (notes[0] && notes[0].id) || ""
+  );
 
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
-    const [state,setState] =useState(()=> console.log('stae intialization')
-    
-    )
-    useEffect(()=>{
-         localStorage.setItem('notes',JSON.stringify(notes));
-    },[notes])
-    
-    function createNewNote() {
-        const newNote = {
-            id: nanoid(),
-            body: "# Type your markdown note's title here"
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "# Type your markdown note's title here",
+    };
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+    setCurrentNoteId(newNote.id);
+  }
+
+  function updateNote(text) {
+  /*   setNotes((oldNotes) =>
+      oldNotes.map((oldNote) => {
+        return oldNote.id === currentNoteId
+          ? { ...oldNote, body: text }
+          : oldNote;
+      })
+    ); */
+
+    setNotes(oldNotes => {
+        const newArray = []
+        for(let i = 0; i < oldNotes.length; i++) {
+            const oldNote = oldNotes[i]
+            if(oldNote.id === currentNoteId) {
+                newArray.unshift({ ...oldNote, body: text })
+            } else {
+                newArray.push(oldNote)
+            }
         }
-        setNotes(prevNotes => [newNote, ...prevNotes])
-        setCurrentNoteId(newNote.id)
-    }
-    
-    function updateNote(text) {
-        setNotes(oldNotes => oldNotes.map(oldNote => {
-            return oldNote.id === currentNoteId
-                ? { ...oldNote, body: text }
-                : oldNote
-        }))
-    }
-    
-    function findCurrentNote() {
-        return notes.find(note => {
-            return note.id === currentNoteId
-        }) || notes[0]
-    }
-    
+        return newArray
+    })
+  }
+
+  function findCurrentNote() {
     return (
-        <main>
-        {
-            notes.length > 0 
-            ?
-            <Split  //used to split sidebar and main tab !!
-                sizes={[30, 70]} 
-                direction="horizontal" 
-                className={styles.split}
-            >
-                <Sidebar
-                    notes={notes}
-                    currentNote={findCurrentNote()}
-                    setCurrentNoteId={setCurrentNoteId}
-                    newNote={createNewNote}
-                />
-                {
-                    currentNoteId && 
-                    notes.length > 0 &&
-                    <Editor 
-                        currentNote={findCurrentNote()} 
-                        updateNote={updateNote} 
-                    />
-                }
-            </Split>
-            :
-            <div className={styles.noNotes}>
-                <h1>You have no notes</h1>
-                <button 
-                    className={styles.firstNote}
-                    onClick={createNewNote}
-                >
-                    Create one now
-                </button>
-            </div>
-            
-        }
-        </main>
-    )
+      notes.find((note) => {
+        return note.id === currentNoteId;
+      }) || notes[0]
+    );
+  }
+
+  function deleteNote(event, noteId) {
+    event.stopPropagation();
+    const newArray = notes.filter((note) => note.id !== noteId);
+    setNotes(newArray);
+  }
+  return (
+    <main>
+      {notes.length > 0 ? (
+        <Split //used to split sidebar and main tab !!
+          sizes={[30, 70]}
+          direction="horizontal"
+          className={styles.split}
+        >
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+            handleDelete={deleteNote}
+          />
+          {currentNoteId && notes.length > 0 && (
+            <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+          )}
+        </Split>
+      ) : (
+        <div className={styles.noNotes}>
+          <h1>You have no notes</h1>
+          <button className={styles.firstNote} onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )}
+    </main>
+  );
 }
--- notes-app/src/components/Sidebar.js
+++ notes-app/src/components/Sidebar.js
@@ -1,27 +1,38 @@
-import React from "react"
-import styles from  '../style.module.css'
-export default function Sidebar(props) {
-    const noteElements = props.notes.map((note, index) => (
-        <div key={note.id}>
-            <div
-                
-                className={`${styles.title} ${
-                    note.id === props.currentNote.id ? styles.selectedNote : ""
-                }`}
-                onClick={() => props.setCurrentNoteId(note.id)}
-            >
-                <h4 className={styles.textSnippet}>Note {index + 1}</h4>
-            </div>
-        </div>
-    ))
+/** @format */
 
-    return (
-        <section className={`${styles.pane} ${styles.sidebar}`}>
-            <div className={styles.sidebarHeader}>
-                <h3>Notes</h3>
-                <button className={styles.newNote} onClick={props.newNote}>+</button>
-            </div>
-            {noteElements}
-        </section>
-    )
+import React from "react";
+import styles from "../style.module.css";
+import { FaTrashAlt } from "react-icons/fa";
+export default function Sidebar(props) {
+  const noteElements = props.notes.map((note, index) => (
+    <div key={note.id}>
+      <div
+        className={`${styles.title} ${
+          note.id === props.currentNote.id ? styles.selectedNote : ""
+        }`}
+        onClick={() => props.setCurrentNoteId(note.id)}
+      >
+        <h4 className={styles.textSnippet}>{note.body.split("\n")[0]}</h4>
+        {note.id === props.currentNote.id  && (
+          <FaTrashAlt
+            onClick={(event)=> props.handleDelete(event,note.id)}
+            color="white"
+            className={styles.deleteIcon}
+          />
+        )}
+      </div>
+    </div>
+  ));
+
+  return (
+    <section className={`${styles.pane} ${styles.sidebar}`}>
+      <div className={styles.sidebarHeader}>
+        <h3 style={{paddingLeft:'5px'}} >Notes</h3>
+        <button className={styles.newNote} onClick={props.newNote}>
+          +
+        </button>
+      </div>
+      {noteElements}
+    </section>
+  );
 }
