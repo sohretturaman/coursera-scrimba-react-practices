4118532cc01ec19e610749c1f1f4ff3dc4e065ce
şöhrett <sohretturaman@gmail.com>
2024-02-08 15:56:39 +0300

    tenzies app's uı has been competed

--- tenzies-project/src/App.js
+++ tenzies-project/src/App.js
@@ -1,79 +1,45 @@
-import React from "react"
-import Confetti from "react-confetti"
-import Die from "./components/Die"
-import AnimatedHeader from "./components/AnimatedHeader"
+import React, { useState } from "react";
+import Die from "./components/Die";
 
-export default function App() {
-    const [dice, setDice] = React.useState(allNewDice())
-    const [tenzies, setTenzies] = React.useState(false)
-    
-    React.useEffect(() => {
-        const firstValue = dice[0].value
-        const allHeld = dice.every(die => die.held)
-        const allSameNumber = dice.every(die => die.value === firstValue)
-        if(allHeld && allSameNumber) {
-            setTenzies(true)
+const App = () => {
+ const [dies,setDies] =useState([]); 
+ 
+
+    const allNewDice =()=>{
+        const randomNumsList=[];
+        for(let i =0; i<10; i ++){
+           let newRandomNum = Math.floor(Math.random()*6)+1
+         
+           randomNumsList.push(newRandomNum);
+           
         }
-    }, [dice])
-    
-    function randomDieValue() {
-        return Math.ceil(Math.random() * 6)
+      
+      setDies(randomNumsList)
     }
 
-    function allNewDice() {
-        const newArray = []
-        for(let i = 0; i < 10; i++) {
-            const newDie = {
-                value: randomDieValue(),
-                held: false,
-                id: i + 1
-            }
-            newArray.push(newDie)
-        }
-        return newArray
-    }
 
-    function rollUnheldDice() {
-        if (!tenzies) {
-            setDice((oldDice) => oldDice.map((die, i) =>
-                die.held ? 
-                    die : 
-                    { value: randomDieValue(), held: false, id: i + 1 }
-            ))
-        } else {
-            setDice(allNewDice())
-            setTenzies(false)
-        }
-    }
+    const diesMap =dies.map((number)=>(
+        <Die value={number} />
+     ))
 
-    function holdDice(id) {
-        setDice(prevDice => prevDice.map(die => {
-            return die.id === id ? 
-                {...die, held: !die.held} : 
-                die
-        }))
-    }
-
-    const diceElements = dice.map((die) => (
-        <Die key={die.id} {...die} hold={() => holdDice(die.id)} />
-    ))
-
-    return (
+  return (
+    <main>
         <div>
-               <main>
-            {tenzies && <Confetti />}
-            <AnimatedHeader/>
-            <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-            <div className="die-container">{diceElements}</div>
-            <button className="roll-dice" onClick={rollUnheldDice}>
-                {tenzies ? "Reset Game" : "Roll"}
-            </button>
-         <div >
-             <p className="buttont">Amazing Job!!</p>
-            
-         </div>
-        </main>
+             <h1>Tenzier</h1>
+             <p style={{width:'90%',fontWeight:500}}>
+             Roll until all dice are the same. 
+             Click each die to freeze it at its current value between rolls.
+             </p>
         </div>
-       
-    )
-}
+         <div className="dieGrid">
+                  {diesMap}
+         </div>
+         <div>
+             <button className="button" onClick={allNewDice}>Click</button>
+         </div>
+        
+    </main>
+  )
+};
+
+export default App;
--- tenzies-project/src/components/Die.js
+++ tenzies-project/src/components/Die.js
@@ -1,14 +1,13 @@
-import React from "react"
+import React from 'react'
 
-export default function Die(props) {
-    console.log('props.value',props.value)
-    const styles = {
-        backgroundColor: props.held ? "#59E391" : "white",
-
-    }
-    return (
-        <div className="die-face" onClick={props.hold} style={styles}>
-            <h2 className="die-num">{props.value}</h2>
-        </div>
-    )
+const Die = ({value}) => {
+    
+    const styles = {backgroundColor:'#59E391'}
+  return (
+    <div className='dieWrapper' style={styles}>
+        <h1>{value}</h1>
+    </div>
+  )
 }
+
+export default Die
